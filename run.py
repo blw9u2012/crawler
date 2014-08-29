65b62e4d58caf95d97d91d6e918794ab00554090
Brandon Walton <brandonlwalton@gmail.com>
2014-08-29 00:31:55 -0400

    added gitignore file

--- run.py
+++ run.py
@@ -8,15 +8,18 @@ from crawler import Crawler
 from CrawlerCache import CrawlerCache
 
 # Set global variables
-num_threads = 4
+num_threads = 3
 feed_queue = Queue(maxsize=0)
 
 # Define a function that each thread will run when start() is called
+
+
 def worker_crawl(q):
     while not q.empty():
         site = q.get()
         print "Getting ready to crawl: " + site
-        crawler = Crawler(CrawlerCache(site[11:-4] + '.db'))
+        # crawler = Crawler(CrawlerCache(site[11:-4] + '.db'))
+        crawler = Crawler(CrawlerCache('crawler.db'))
         print "Successfully created crawler for site: " + site + " with object: " + str(crawler)
 
         # Crawl the site
@@ -27,10 +30,17 @@ def worker_crawl(q):
 
 if __name__ == "__main__":
     root_re = re.compile('^/$').match
-    # Adding the sites manually to the queue is done for the sake of simplicity
-    feed_queue.put("http://www.woot.com")
-    feed_queue.put("http://www.cowboom.com")
-    feed_queue.put("http://www.slickdeals.net")
+    # Adding the sites manually to the queue is done for the sake of
+    # simplicity. The queue is a FIFO queue.
+    get_sites = True
+    print "Please enter sites one at a time. When finished enter 'done'"
+    while get_sites:
+        site = raw_input("@> ")
+        if site.lower() == "done":
+            get_sites = False
+            print "Finished entering sites"
+        else:
+            feed_queue.put(site)
 
     # Create the worker threads, feed them the worker function and the queue
     start = time.clock()
